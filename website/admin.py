3be7acadfcf9193823057d3ff833063eec1cd40a
sumdy <395747812@qq.com>
2017-11-10 17:01:36 +0800

    6666

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Article, Person
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+	"""docstring for ArticleAdmin"""
+	list_display = ('title','pub_date','update_time',)
+		
+class PersonAdmin(admin.ModelAdmin):
+	"""docstring for PersonAdmin"""
+	list_display = ('full_name',)
+		
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Person, PersonAdmin)
--- website/models.py
+++ website/models.py
@@ -1,11 +1,36 @@
+# coding:utf-8
+from __future__ import unicode_literals
+ 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
 class Person(models.Model):
 	"""docstring for Person"""
 	name = models.CharField(max_length=30)
 	age = models.IntegerField()
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
 
 	def __str__(self):
 		return self.name
+
+	def my_property(self):
+		return self.first_name + ' ' + self.last_name
+	my_property.short_description = 'Full name of the person'
+
+	full_name = property(my_property)
+
+
+
+class Article(models.Model):
+	title = models.CharField(u'标题',max_length=256)
+	content = models.TextField(u'内容')
+
+	pub_date = models.DateTimeField(u'发表时间', auto_now_add=True, editable=True)
+	update_time = models.DateTimeField(u'更新时间', auto_now=True, null=True)
+
+	def __str__(self):
+		return self.title
+
 		
